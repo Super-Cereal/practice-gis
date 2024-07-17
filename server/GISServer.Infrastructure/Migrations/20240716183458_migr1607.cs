c26c217ac27bb5bdf92d0f5102e04bfcc9ac9518
Isa <murtazaevstudy@gmail.com>
2024-07-17 15:06:56 +0300

    Добавлена функция установки топологической связи между объектами. Фикс: создание объекта с минимальной инфой. Добавлена миграция.

---
+++ server/GISServer.Infrastructure/Migrations/20240716183458_migr1607.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GISServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class migr1607 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ server/GISServer.Infrastructure/Migrations/20240717115755_migr1707.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GISServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class migr1707 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "GeoClassifiers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Code",
+                table: "GeoClassifiers",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
