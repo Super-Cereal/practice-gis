32b0d518dfc109eaa2e5e59f02a1fd7ab3f78215
softqwak <valentin.kuzichev@yandex.ru>
2024-07-18 21:20:06 +0300

    refact 03

---
+++ server/GISServer.API/Service/IGeoObjectClassifiersService.cs
@@ -0,0 +1,11 @@
+﻿using GISServer.API.Model;
+using GISServer.Domain.Model;
+
+namespace GISServer.API.Service
+{
+    public interface IGeoObjectClassifiersService
+    {
+        public Task<List<GeoObjectsClassifiers>> GetGeoObjectsClassifiers(Guid? geoObjectInfoId);
+        public Task<List<GeoObjectsClassifiers>> GetGeoObjectsClassifiers();
+    }
+}
