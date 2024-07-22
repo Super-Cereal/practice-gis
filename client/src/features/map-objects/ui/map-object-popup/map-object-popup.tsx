0d1ec0feb9e392cb17d3db878a6a7e185576b1b5
egor-kuch <egor-kuch@yandex-team.ru>
2024-07-23 00:27:57 +0300

    Отображение аспекта в попапе

--- client/src/features/map-objects/ui/map-object-popup/map-object-popup.tsx
+++ client/src/features/map-objects/ui/map-object-popup/map-object-popup.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Popup } from 'react-leaflet';
+import { useUnit } from 'effector-react';
 
-import { GEO_OBJECT_STATUS, type GeoObject, type GeometryGeoJSON, getCenterByCoords } from '../../../../entities/geoobject';
+import {
+    GEO_OBJECT_STATUS,
+    type GeoObject,
+    type GeometryGeoJSON,
+    aspectsModel,
+    getCenterByCoords,
+} from '../../../../entities/geoobject';
+import { mapModel } from '../../../../entities/map';
 import { useTimezoneAndLocalTime } from '../../../../shared/lib/time';
 import { TextWithCopy } from '../../../../shared/ui/text-with-copy';
 
@@ -27,11 +35,18 @@ export const MapObjectPopup = (props: Props) => {
 const Content = ({ object, geometry: { type, coordinates } }: Props) => {
     const { id, name, status, geoObjectInfo } = object;
 
+    const assignedAspects = useUnit(aspectsModel.$assignedAspects);
+    const selectedAspect = useUnit(mapModel.$mapAspect);
+
+    const geoobjectAspect = assignedAspects.find(
+        ({ code, geographicalObjectId }) => code === selectedAspect?.code && geographicalObjectId === id,
+    );
+
     const { timezone, localTime } = useTimezoneAndLocalTime(getCenterByCoords(coordinates), id);
 
-    const getStatusKey = (status: number) => {
-        return Object.entries(GEO_OBJECT_STATUS).find(([_, value]) => value === status)?.[0];
-    };
+    const getStatusKey = (status: number) =>
+        Object.entries(GEO_OBJECT_STATUS).find(([_, value]) => value === status)?.[0];
+
     return (
         <>
             <h3 className={styles.title}>
@@ -39,7 +54,15 @@ const Content = ({ object, geometry: { type, coordinates } }: Props) => {
             </h3>
             <TextWithCopy title="id" text={id} />
 
-            <div className={styles.content}>{geoObjectInfo?.commonInfo}</div>
+            <div className={styles.content}>
+                <div>{geoObjectInfo?.commonInfo}</div>
+
+                {geoobjectAspect && (
+                    <div className={styles.aspect}>
+                        <b>{geoobjectAspect.type}:</b> {geoobjectAspect.endPoint}
+                    </div>
+                )}
+            </div>
 
             <div className={styles.bottom}>
                 <div className={styles.textBlock}>
@@ -55,11 +78,11 @@ const Content = ({ object, geometry: { type, coordinates } }: Props) => {
                     <b>Язык:</b> {geoObjectInfo?.language}
                 </span>
 
-                {object.status &&
+                {status && (
                     <span>
-                        <b>Статус:</b> {getStatusKey(object.status)}
+                        <b>Статус:</b> {getStatusKey(status)}
                     </span>
-                }
+                )}
             </div>
         </>
     );
