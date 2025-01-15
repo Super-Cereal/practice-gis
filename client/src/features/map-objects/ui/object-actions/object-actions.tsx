f55e3c84df02109ef537ccac9ac3f2265087ff93
AlexandraDDD <121177907+AlexandraDDD@users.noreply.github.com>
2025-01-15 09:37:47 +0300

    f

--- client/src/features/map-objects/ui/object-actions/object-actions.tsx
+++ client/src/features/map-objects/ui/object-actions/object-actions.tsx
@@ -9,7 +9,6 @@ import { mapObjectsModel } from '../../lib/map-objects.model';
 import { mapModel } from '../../../../entities/map';
 import { editorModel } from '../../../map-editor/lib/editor.model';
 
-
 interface MapObjectItemProps {
     geoObject: GeoObject;
     parentGeoObjects: (GeoObject | undefined)[];
@@ -46,34 +45,27 @@ export const MapObjectItem: React.FC<MapObjectItemProps> = ({ geoObject, parentG
     const handleDelete = async (geoObject: GeoObject) => {
         await mapObjectsModel.removeSelectedGeoobject(geoObject);
         await geoObjectModel.deleteGeoObjectFx(geoObject.id);
-
     };
 
     const handleUnSelect = async (geoObject: GeoObject) => {
         await mapObjectsModel.removeSelectedGeoobject(geoObject);
     };
 
-
     const handleClipping = (geoObject: GeoObject) => {
         if (!isClippingMode) {
-            handleZoom(geoObject)
-            mapModel.setEditorPointsOnCorners(false)
-            editorModel.setClippedObject(geoObject)
-
+            handleZoom(geoObject);
+            mapModel.setEditorPointsOnCorners(false);
+            editorModel.setClippedObject(geoObject);
         } else {
-            editorModel.setClippedObject(null)
+            editorModel.setClippedObject(null);
         }
-        mapModel.setClippingMode(!isClippingMode)
+        mapModel.setClippingMode(!isClippingMode);
     };
     const handleDescription = (geoObject: GeoObject) => {
         geoObjectFormModel.setSelectedObjectToEdit(geoObject);
 
-        mapModel.setDescriptionMode(true)
+        mapModel.setDescriptionMode(true);
     };
-
-
-
-
 
     return (
         <div className={styles.wrapper}>
@@ -92,11 +84,11 @@ export const MapObjectItem: React.FC<MapObjectItemProps> = ({ geoObject, parentG
                 <Button mix={styles.btn} onClick={() => handleDescription(geoObject)}>
                     Раскрыть описание
                 </Button>
-                {mapEditable &&
+                {mapEditable && (
                     <Button mix={styles.btn} onClick={() => handleClipping(geoObject)}>
                         {isClippingMode ? 'Отключить редактор' : 'Редактировать местность'}
                     </Button>
-                }
+                )}
 
                 <Button mix={styles.btn} onClick={handleUpdateModalFormOpen}>
                     Изменить геообъект
